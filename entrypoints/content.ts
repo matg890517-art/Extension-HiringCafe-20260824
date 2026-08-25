67f4566bc2f6384f9d6e5c7a7636f5238de5efc6
woody <mat.g.890517@gmail.com>
2026-08-25 16:44:58 -0700

    feat: add Material-UI and Emotion dependencies; update wxt config for permissions

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,20 @@
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
+  matches: ['*://hiringcafe.com/*', '*://*.hiringcafe.com/*' , 'https://hiringcafe.com/'],
   main() {
-    console.log('Hello content.');
+    console.log('Hello content.', location.href);
+
+    // const container = document.createElement('span'); 
+    // document.body.append(container); 
+
+    browser.runtime.onMessage.addListener((message, _sender, sendResponse) => {
+      console.log('received[content]', message);
+      if (message?.type !== 'GET_JOB') return;
+      const drawer = document.querySelector(
+        'div[role="dialog"][aria-modal="true"].chakra-modal__content',
+      );
+      sendResponse(
+        drawer ? { ok: true } : { ok: false, error: 'no drawer' },
+      );
+    });
   },
 });
--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,12 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  manifest: {
+    permissions: ['storage', 'tabs', 'sidePanel','scripting'],
+    host_permissions: [
+      '*://hiringcafe.com/*',
+      '*://*.hiringcafe.com/*',
+      'http://127.0.0.1:8980/*',
+    ],
+  },
 });
